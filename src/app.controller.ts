b6d29b52c607725a6fdfd345c2d3f1a4e36ba60e
Christiankim <christian@swit.io>
2023-09-14 12:33:12 +0900

    add ngrok

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/callback')
+  callback(@Req() request: Request): string {
+    new Logger('callback').log(request.query.code);
+    const code = request.query.code as string;
+    return code;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NgrokSessionBuilder } from '@ngrok/ngrok';
+import { Logger } from '@nestjs/common';
+
+// Please provide your ngrok authtoken here.
+const ngRokToken = '';
 
 async function bootstrap() {
+  const port = 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+
+  // Setup ngrok
+  const session = await new NgrokSessionBuilder()
+    .authtoken(ngRokToken)
+    .connect();
+  const tunnel = await session.httpEndpoint().listen();
+  new Logger('main').log(`Ingress established at ${tunnel.url()}`);
+  tunnel.forward(`localhost:${port}`);
 }
 bootstrap();
